6f5dd7a1b6eccb9eae295a4944dd9086fc4cc5dc
minlag <102268873+minlag@users.noreply.github.com>
2024-01-04 00:21:35 -0500

    Update main.js
    Fixed background scaling error in very narrow windows.

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
-window.onload = function() {
+window.addEventListener('load', function() {
     var pane = document.getElementsByClassName('center-text')[0];
     var mesh = document.getElementById('meshGradient');
     if(window.innerHeight > pane.offsetHeight){
         mesh.style.height = window.innerHeight + 'px';
     } else {
     mesh.style.height = pane.offsetHeight + 'px';
-
-    console.log(mesh.offsetHeight);
-    console.log(pane.offsetHeight);
-    console.log(window.innerHeight);
-    console.log(document.getElementById('center')); 
-    console.log(document.getElementById('meshGradient')); 
     }
- }
+    if(mesh.offsetWidth <= pane.offsetWidth){
+        mesh.style.width = 120 + 'vw';
+    }
+    else{
+        mesh.offsetWidth = mesh.style.width = window.innerWidth + 'px';
+    }
+ });
 
 window.addEventListener('resize', function() {
     var pane = document.getElementsByClassName('center-text')[0];
@@ -21,10 +21,11 @@ window.addEventListener('resize', function() {
         mesh.style.height = window.innerHeight + 'px';
     } else {
     mesh.style.height = pane.offsetHeight + 'px';
-    console.log(mesh.offsetHeight);
-    console.log(pane.offsetHeight);
-    console.log(window.innerHeight);
-    console.log(document.getElementById('center')); 
-    console.log(document.getElementById('meshGradient')); 
+    }
+    if(mesh.offsetWidth <= pane.offsetWidth){
+        mesh.style.width = 120 + 'vw';
+    }
+    else{
+        mesh.offsetWidth = mesh.style.width = window.innerWidth + 'px';
     }
 });
